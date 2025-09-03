c40bcd6744b3091c4aacb943d09b8f6ff203924e
David Willman <80082754+CrazzyCrisis@users.noreply.github.com>
2025-09-03 10:35:07 -0400

    Testing Ant PNG

--- sketch.js
+++ sketch.js
@@ -1,8 +1,23 @@
+
+let img;
+
+// Load the image and create a p5.Image object.
+function preload() {
+  img = loadImage('/Entities/Ants/Ant.png');
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHight);
+
+  // Draw the image.
+  image(img, 0, 0);
+
+  describe('Image of the underside of a white umbrella and a gridded ceiling.');
 }
 
 function draw() {
   background(220);
   ellipse(200, 200, 50, 50);
 }
+
+
