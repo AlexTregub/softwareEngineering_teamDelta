156adce7188924fb2b9628d8971d702adb225254
afabiku <fafabikualex@gmail.com>
2025-09-04 13:34:05 -0400

    Optimized terrain generation, dirt made in patches

--- sketch.js
+++ sketch.js
@@ -1,70 +1,88 @@
 let initialize = false;
 let canvasX = 800;
 let canvasY = 800;
-let gridList = {};
+let gridList = {}; // {'x_y' : Grid object}
 
 let grassImg;
 let dirtImg;
-function preload(){
-  grassImg = loadImage('Images/grass.jpg');
+
+function preload() {
+  grassImg = loadImage('Images/grass.png');
   dirtImg = loadImage('Images/dirt.png');
+
+
 }
 
+function coordKey(x, y) {
+  return `${x}_${y}`;
+}
 
 class Grid {
-  constructor(x,y,gridSize){
+  constructor(x, y, gridSize) {
     this.x = x;
     this.y = y;
     this.gridSize = gridSize;
-    this.materials = {'grass':10,'dirt':300}; // Material Rarity
-    this.materialImages = {
-      'grass': () => image(grassImg, this.x, this.y,this.gridSize, this.gridSize),
-      'dirt': () => image(dirtImg, this.x, this.y, this.gridSize, this.gridSize)
-    };
+    this.materials = { 'grass': 50, 'dirt': 20,}; // Material Rarity
+    this.material = this.getMaterial();
   }
 
-  setMaterial(material){
-    if(this.materialImages[material]){
-      this.materialImages[material]();
-    }
-  }
-
-  getMaterial(){
+  getMaterial() {
     let totalRarity = 0;
-    for (let rarity in this.materials){
+    for (let rarity in this.materials) {
       totalRarity += this.materials[rarity];
     }
 
     let randomValue = random() * totalRarity;
-    for(let materialType in this.materials){
+    for (let materialType in this.materials) {
       randomValue -= this.materials[materialType];
-      if(randomValue <= 0){
+      if (randomValue <= 0) {
         return materialType;
       }
     }
     return 'grass';
   }
 
+  draw() {
+    // If neighbor is dirt, force this one to dirt
+    let neighborsMaterials = [
+      gridList[coordKey(this.x - this.gridSize, this.y)], 
+      gridList[coordKey(this.x + this.gridSize, this.y)], 
+      gridList[coordKey(this.x, this.y - this.gridSize)], 
+      gridList[coordKey(this.x, this.y + this.gridSize)]  
+    ];
 
-  
-  draw(){
-    let randomMaterial = this.getMaterial();
-    this.setMaterial(randomMaterial);
-    // square(this.x,this.y,this.gridSize);
+    for (let n of neighborsMaterials) {
+      if (n && n.material === 'dirt') {
+        if (random() < 0.4) { 
+          this.material = 'dirt';
+        }
+      }
+    }
+
+
+    // Draw image based on material
+    if (this.material == 'grass') {
+      image(grassImg, this.x, this.y, this.gridSize, this.gridSize);
+    } else if (this.material == 'dirt') {
+      image(dirtImg, this.x, this.y, this.gridSize, this.gridSize);
+    }
   }
 }
 
-// Logic needs checked to prevent overlapping
 function loadMap() {
   let gridCount = 20;
-  let gridSize = canvasX/gridCount;
+  let gridSize = canvasX / gridCount;
 
-  for(let i = 0; i < gridSize; ++i){
-    for(let j = 0; j < gridSize; ++j){
-      let object = new Grid(i*gridSize,j*gridSize,gridSize);
-      object.draw();
-      gridList[object] = (object.x,object.y); // ??? Use to refrence back to a specific grid
+  for (let i = 0; i < gridCount; ++i) {
+    for (let j = 0; j < gridCount; ++j) {
+      let object = new Grid(i * gridSize, j * gridSize, gridSize);
+      gridList[coordKey(object.x, object.y)] = object;
     }
+  }
+
+  // Draw everything
+  for (let key in gridList) {
+    gridList[key].draw();
   }
 }
 
@@ -73,9 +91,8 @@ function setup() {
 }
 
 function draw() {
-  if(!initialize){
+  if (!initialize) {
     loadMap();
     initialize = true;
   }
 }
-
