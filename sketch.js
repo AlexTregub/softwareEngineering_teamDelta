82a6383b4b164381e83147e32673f25b7e21eb2d
afabiku <fafabikualex@gmail.com>
2025-09-05 19:32:38 -0400

    Merge AF and AT code

--- sketch.js
+++ sketch.js
@@ -1,98 +1,169 @@
 let initialize = false;
 let canvasX = 800;
 let canvasY = 800;
-let gridList = {}; // {'x_y' : Grid object}
+let gridList = {};
+
+let seed;
+let map;
 
 let grassImg;
 let dirtImg;
-
-function preload() {
-  grassImg = loadImage('Images/grass.png');
+function preload(){
+  grassImg = loadImage('Images/grass.jpg');
   dirtImg = loadImage('Images/dirt.png');
-
-
 }
 
-function coordKey(x, y) {
-  return `${x}_${y}`;
+
+////// TERRAIN
+// FIRST IN PAIR IS PROBABILITY, SECOND IS RENDER FUNCTION
+// NOTE: PROBABILITIES SHOULD BE IN ORDER: LEAST->GREATEST. 'REAL' PROBABILITY IS (A_i - A_(i-1)).
+// LAST IS DEFAULT aka PROB=1
+let TERRAIN_MATERIALS = { // All-in-one configuration object.
+  'stone' : [0.05, (x,y,squareSize) => {fill(77,77,77); strokeWeight(0);rect(x,y,squareSize,squareSize);}], // Example of more advanced lambda.
+  'dirt' : [0.4 , (x,y,squareSize) => image(dirtImg, x, y, squareSize, squareSize)],
+  'grass' : [1 , (x,y,squareSize) => image(grassImg, x, y, squareSize,squareSize)],
+};
+
+class Terrain {
+  constructor(tileCountX, tileCountY, tileSize) {
+    // Config...
+    this._xCount = tileCountX;
+    this._yCount = tileCountY;
+    this._tileSize = tileSize;
+
+    // Initialize 1d _tileStore
+    this._tileStore = [];
+    for (let j = 0; j < this._yCount; ++j) { // ... then Y...
+      for (let i = 0; i < this._xCount; ++i) { // row of X first... ^
+        this._tileStore.push( // Add tile to...
+          new Tile(
+            i*this._tileSize,j*this._tileSize, // x,y position conversion.
+            this._tileSize
+          )
+        );
+      }
+    }
+  }
+
+
+
+  //// Utility
+  conv2dpos(posX,posY) { // Converts 2d -> 1d position
+    return posX + this._xCount*posY;
+  }
+
+
+
+  //// Access
+  setTile(posX,posY,material) {
+    return this._tileStore[this.conv2dpos(posX,posY)].setMaterial(material);
+  }
+
+  getTile(posX,posY) {
+    return this._tileStore[this.conv2dpos(posX,posY)].getMaterial();
+  }
+
+
+
+  //// Usage
+  randomize(seed) { // Randomize all values via set seed
+    randomSeed(seed); // Set global seed.
+
+    for (let i = 0; i < this._xCount*this._yCount; ++i) {
+      this._tileStore[i].randomizeMaterial(this._tileStore); // Rng calls should use global seed
+    }
+  }
+
+  render() { // Render all tiles
+    for (let i = 0; i < this._xCount*this._yCount; ++i) {
+      this._tileStore[i].render();
+    }
+  }
 }
 
-class Grid {
-  constructor(x, y, gridSize) {
-    this.x = x;
-    this.y = y;
-    this.gridSize = gridSize;
-    this.materials = { 'grass': 50, 'dirt': 20,}; // Material Rarity
-    this.material = this.getMaterial();
+class Tile { // Similar to former 'Grid'. Now internally stores material state.
+  constructor(renderX,renderY,tileSize) {
+    // Internal coords
+    this._x = renderX;
+    this._y = renderY;
+
+    this._squareSize = tileSize;
+
+    // Texture Properties
+    this._materialSet = 'grass'; // Used for storage of randomization. Initialized as default value for now
+  }
+ 
+
+  //// Access/usage
+  // Will select random material for current tile. No return.
+  // Higher chance for dirty to appear near other dirt blocks
+  randomizeMaterial(tileStore) { 
+    let selected = random(); // [0-1)
+    for (let checkMat in TERRAIN_MATERIALS) {
+      if (selected < TERRAIN_MATERIALS[checkMat][0]) { // Fixed less-than logic
+        this._materialSet = checkMat;
+        break;
+      }
+    }
+
+    
+    for (let tile of tileStore) {
+      let size = this._squareSize;
+      let left = (tile._x == this._x - size && tile._y == this._y); //  if theres a tile to the left
+      let right = (tile._x == this._x + size && tile._y == this._y);  // if theres a tile to the right
+      let above = (tile._x == this._x && tile._y == this._y - size);  // if theres a tile above
+      let below = (tile._x == this._x && tile._y == this._y + size);    // if theres a tile below
+
+      if(left || right || above || below){
+        let rand = random();
+        if (tile.getMaterial() == "dirt") {
+          if(rand < .4){
+            this._materialSet = "dirt";
+            return; 
+          }
+        }
+      }
+
+
+    }
   }
 
   getMaterial() {
-    let totalRarity = 0;
-    for (let rarity in this.materials) {
-      totalRarity += this.materials[rarity];
+    return this._materialSet;
+  }
+  
+  setMaterial(matName) { // Returns success / fail.
+    if (TERRAIN_MATERIALS[matName]) {
+      this._materialSet = matName;
+      return true;
     }
-
-    let randomValue = random() * totalRarity;
-    for (let materialType in this.materials) {
-      randomValue -= this.materials[materialType];
-      if (randomValue <= 0) {
-        return materialType;
-      }
-    }
-    return 'grass';
+    return false;
   }
 
-  draw() {
-    // If neighbor is dirt, force this one to dirt
-    let neighborsMaterials = [
-      gridList[coordKey(this.x - this.gridSize, this.y)], 
-      gridList[coordKey(this.x + this.gridSize, this.y)], 
-      gridList[coordKey(this.x, this.y - this.gridSize)], 
-      gridList[coordKey(this.x, this.y + this.gridSize)]  
-    ];
-
-    for (let n of neighborsMaterials) {
-      if (n && n.material === 'dirt') {
-        if (random() < 0.4) { 
-          this.material = 'dirt';
-        }
-      }
-    }
-
-
-    // Draw image based on material
-    if (this.material == 'grass') {
-      image(grassImg, this.x, this.y, this.gridSize, this.gridSize);
-    } else if (this.material == 'dirt') {
-      image(dirtImg, this.x, this.y, this.gridSize, this.gridSize);
-    }
+  render() { // Render, previously draw
+    TERRAIN_MATERIALS[this._materialSet][1](this._x,this._y,this._squareSize); // Call render lambda
+    return;
   }
+
+
 }
 
-function loadMap() {
-  let gridCount = 20;
-  let gridSize = canvasX / gridCount;
 
-  for (let i = 0; i < gridCount; ++i) {
-    for (let j = 0; j < gridCount; ++j) {
-      let object = new Grid(i * gridSize, j * gridSize, gridSize);
-      gridList[coordKey(object.x, object.y)] = object;
-    }
-  }
-
-  // Draw everything
-  for (let key in gridList) {
-    gridList[key].draw();
-  }
-}
-
+////// MAIN
 function setup() {
   createCanvas(canvasX, canvasY);
 }
 
 function draw() {
-  if (!initialize) {
-    loadMap();
+  if(!initialize){
+    seed = hour()*minute()*floor(second()/10); // Have seed change every 10 sec.
+
+    map = new Terrain(8,8,100); // Hardcoded. In the future, make automatic.
+    map.randomize(seed); // Randomize with set seed
+
     initialize = true;
   }
+
+  map.render(); // Each call will re-render configuration of map
 }
+
