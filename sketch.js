98c72fd4cca347e5662d398193eefb51612c9fad
Colin <crgrant24@gmail.com>
2025-09-10 19:31:27 -0400

    Adding more logic documentation

--- sketch.js
+++ sketch.js
@@ -10,6 +10,12 @@ function mousePressed() {
   for (let i = 0; i < ants.length; i++) {
     if (ants[i].isMouseOver(mouseX, mouseY)) { //Eventually make it open some interface menu
       ants[i].moveToLocation(mouseX + 50, mouseY + 50);
+      /*
+      Once an ant is clicked, makePath in the ants class is called
+      and repeatedly draws lines to the corresponding grid tile
+      repeatedly until a valid destination is clicked. Then it calls
+      a movement function and the ant moves there.
+      */
       return; // Stop after first match
     }
   }
