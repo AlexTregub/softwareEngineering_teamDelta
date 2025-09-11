7e18655580adc80f12d9bcd6665d9962cc1edd44
Colin <crgrant24@gmail.com>
2025-09-10 22:09:12 -0400

    properly stores start and end tile to call getPath()

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ let canvasX = 800;
 let canvasY = 800;
 let recordingPath = false;
 let selectedAnt = null;
+let startTile;
+let endTile;
 
 function preload(){
   terrainPreloader()
@@ -9,18 +11,28 @@ function preload(){
 }
 
 function mousePressed() {
-  for (let i = 0; i < ants.length; i++) {
-    if (ants[i].isMouseOver(mouseX, mouseY)) { //Eventually make it open some interface menu
-      if(!recordingPath){
+  if(!recordingPath){
+    for (let i = 0; i < ants.length; i++) {
+      if (ants[i].isMouseOver(mouseX, mouseY)) { //Eventually make it open some interface menu
         recordingPath = true;
         selectedAnt = ants[i];
         let antTileX = floor(selectedAnt.getCenter().x / map._tileSize);
         let antTileY = floor(selectedAnt.getCenter().y / map._tileSize);
-        let startTile = map._tileStore[map.conv2dpos(antTileX, antTileY)];
+        this.startTile = map._tileStore[map.conv2dpos(antTileX, antTileY)];
+      }
+      if(recordingPath){
+        endTileX = floor(mouseX / map._tileSize);
+        endTileT = floor(mouseY / map._tileSize);
+        this.endTile = map._tileStore[map.conv2dpos(endTileX, endTileY)];
+        getPath(startTile, endTile);
+        recordingPath = false;
+        selectedAnt = null;
+        startTile = null;
       }
       
 
       /*
+      Eventually...
       Once an ant is clicked, makePath in the ants class is called
       and repeatedly draws lines to the corresponding grid tile
       repeatedly until a valid destination is clicked. Then it calls
