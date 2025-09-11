bcc2d3f8696ddf9e7d8c72eae9e61276f0fd6a66
Colin <crgrant24@gmail.com>
2025-09-11 01:59:25 -0400

    cleaning up

--- Classes/pathfinding.js
+++ Classes/pathfinding.js
@@ -1,7 +1,3 @@
-let canvasX = 800;
-let canvasY = 800;
-let rows = 25;
-let columns = 25;
 let grid = new Array(rows);
 let openList = [];
 let closedList = [];
@@ -11,13 +7,8 @@ let start;
 let end;
 let pathFound = false;
 
-function getPath(start, end){
-  if(checkPoint(start) && checkPoint(end)){ //Ensures start and end are valid
-
-  }
-}
-
 function checkPoint(posX,posY){ //Makes sure a certain square is not a barrier
+  let material = map.getTile()
   if (getTile(posX, posY) == 'badness'){ //Change later depending on map setup
     return false;
   }
@@ -37,7 +28,7 @@ The speed to travel a tile should be the weight * speed * bonus
 multiplier. DO NOT JUST MEASURE BY WEIGHT. NOT ALL ANTS ARE
 CREATED EQUAL
 */
-class Tile{
+class pathNode{
   constructor(i,j){
     this.i = i;
     this.j = j;
@@ -47,14 +38,9 @@ class Tile{
     this.neighbors = []; //N,E,S,W Non-wall tiles
     this.wall = false;
     this.prev = undefined; //To backtrack to start
-
-    this.wallChange = random(1);
-    if(this.wallChange < 0.2){ //20% chance of a wall
-      this.wall = true;
-    }
   }
 
-  setNeighbors(grid){
+  setNeighbors(tileOfInterest){
     let i = this.i;
     let j = this.j;
     if(i > 0){
@@ -77,23 +63,6 @@ function distanceFinder(start, end){
   return abs(start.i - end.i) + abs(start.j - end.j); //Manhattan distance formula
 }
 
-function makeGrid(){
-  for(let i = 0; i < rows; i++){
-    grid[i] = new Array(columns);
-  }
-  for(let i = 0; i < rows; i++){
-    for(let j = 0; j < columns; j++){
-      grid[i][j] = new Tile(i,j);
-    }
-  }
-  for(let i = 0; i < rows; i++){
-    for(let j = 0; j < columns; j++){
-      grid[i][j].setNeighbors(grid);
-    }
-  }
-  //Makes the grid, makes each tile a Tile(), and sets neighbors automatically
-}
-
 function makePath(endNode){
   let path = [];
   let temp = endNode;
@@ -112,35 +81,10 @@ function makePath(endNode){
   }
 }
 
-function setup(){
-  createCanvas(canvasX, canvasY);
-  width = canvasX / columns;
-  height = canvasY / rows; //Initializes size of tiles when painting
-  makeGrid();
-  start = grid[0][0];
-  end = grid[rows-1][columns-1];
-  start.wall = false;
-  end.wall = false; //Makes sure start and end aren't walls. Change in game so not OOB
-  openList.push(start);
-}
-
-function draw(){
-  background(255);
-  for (let i = 0; i < rows; i++){
-    for (let j = 0; j < columns; j++){
-      let tile = grid[i][j]; //Sets tile to current location
-      if (tile.wall){
-        fill(0);
-      }
-      else{
-        fill(255);
-      }
-      stroke(0);
-      rect(tile.j * width, tile.i * height, width, height);
-      //Paints each tile white or black if a wall or not
-    }
+function getPath(startTile, endTile){
+  if(!checkPoint(startTile) || !checkPoint(endTile)){ //Ensures start and end are valid
+    return;
   }
-
   if(openList.length > 0){
     let lowestIndex = 0;
     for(let i = 0; i < openList.length; i++){
@@ -187,28 +131,11 @@ function draw(){
       }
     }
   }
-
-  for(let block of openList){
-    fill(0, 255, 0);
-    rect(block.j * width, block.i * height, width, height);
-  } //openList tiles green
-
-  for(let block of closedList){
-    fill(255, 0, 0);
-    rect(block.j * width, block.i * height, width, height);
-  } //closedList tiles red
-
   if(pathFound){
     makePath(end); //Builds and paints path once completed
   }
-
   if(!pathFound && openList.length == 0){
     noLoop();
     return;
   }
-
-  fill(20, 255, 200);
-  rect(start.j * width, start.i * height, width, height);
-  fill(255, 20, 200);
-  rect(end.j * width, end.i * height, width, height);
 }
