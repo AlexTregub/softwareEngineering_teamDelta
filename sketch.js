f7cfc3dbbd27a0525bdb30484ef74833eb5c4206
Colin <crgrant24@gmail.com>
2025-09-11 01:59:25 -0400

    cleaning up

--- sketch.js
+++ sketch.js
@@ -16,9 +16,10 @@ function mousePressed() {
       if (ants[i].isMouseOver(mouseX, mouseY)) { //Eventually make it open some interface menu
         recordingPath = true;
         selectedAnt = ants[i];
-        let antTileX = floor(selectedAnt.getCenter().x / map._tileSize);
-        let antTileY = floor(selectedAnt.getCenter().y / map._tileSize);
+        let antTileX = floor(selectedAnt.GetCenter().x / map._tileSize);
+        let antTileY = floor(selectedAnt.GetCenter().y / map._tileSize);
         startTile = map._tileStore[map.conv2dpos(antTileX, antTileY)];
+        return;
       }
       /*
       Eventually...
@@ -27,22 +28,22 @@ function mousePressed() {
       repeatedly until a valid destination is clicked. Then it calls
       a movement function and the ant moves there.
       */
-      return; // Stop after first match
     }
   }
   if(recordingPath){
     let endTileX = floor(mouseX / map._tileSize);
     let endTileY = floor(mouseY / map._tileSize);
     endTile = map._tileStore[map.conv2dpos(endTileX, endTileY)];
-    //getPath(startTile, endTile);
+    getPath(startTile, endTile);
 
-    if(selectedAnt && endTile){
-      selectedAnt.moveToLocation(endTile.x, endTile.y);
-    }
+    /*if(selectedAnt && endTile){
+      selectedAnt.moveToLocation(endTile._x, endTile._y); //Moves ant directly to tile
+    }*/
 
     recordingPath = false;
     selectedAnt = null;
     startTile = null;
+    return;
   }
 }
 
