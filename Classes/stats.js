36cd677181f5a3460b687affc688c9f3c87a02a0
CrazzyCrisis <80082754+CrazzyCrisis@users.noreply.github.com>
2025-09-12 15:15:55 -0400

    Finished base stat class, add Exp map object **These are basically dictionaries**, added several exp types, tested map inside of the stats class to ensure it works

--- Classes/stats.js
+++ Classes/stats.js
@@ -1,127 +1,95 @@
-// Stores info for experiance gathered for ants
+function test_stats() {
+    _stats = new stats
+    _stats.test_Exp()
+    expTotalFromAllEntities = new stat("Total World EXP")
+}
+
 
 class stats {
     // tracks the exp gained from all entites in the world
-    static expTotalFromAllEntities
+    // maps work like dicts, but the key doesn't need to be a string or int
+    exp = new Map()
+
+    constructor(){
+        this.createExpMap()
+    }
     
     // returns the total EXP value from all properties
     expTotal
     getExpTotal(){
-        setExpTotal()
+        this.setExpTotal()
         return this.expTotal;
     }
     setExpTotal(){
-        this.expTotal = getExpRanged()+getExpConstruction()+getExpFarming()
-                    + getExpScout()+getExpSwim()+getExpHunt()+getExpGather()+getExpLifetime()
+        this.expTotal = expLifetime.getStatValue + expGather + expHunt
     }
 
-    
-    // exp gained from being alive, up to a max
-    expLifetime
-    getExpLifetime() {
-    return this.expLifetime
+    // Mappings
+    createExpMap(){
+        this.exp.set("Lifetime",new stat("Lifetime EXP")) // exp gained from being alive, up to a max
+        this.exp.set("Gathering",new stat("Gathering EXP")) // exp gained from gathering materials from leaves and such
+        this.exp.set("Hunting",new stat("Hunting EXP")) //exp gained from combat with other ants
+        this.exp.set("Swimming",new stat("Swimming EXP")) //exp gained from swimming and water based activities
+        this.exp.set("Farming",new stat("Farming EXP")) // exp gained from an ant farming aphids, harvesting dew from any source
+        this.exp.set("Construction",new stat("Construction EXP")) // exp gained from an ant building or repairing any structure
+        this.exp.set("Ranged",new stat("Ranged EXP"))  //exp gained from ants that spits and that successfully connects with an valid object
+        this.exp.set("Scouting",new stat("Scouting EXP")) // exp gained from an ant scouting tasks
     }
-    setExpLifetime(value) {
-    this.expLifetime = value
-    if (this.expLifetime < 0) this.expLifetime = 0
+    test_Map(map) {
+        for (const [key, value] of map) {
+            console.log(`${key}: ${value}`);
+        }
     }
-
-
-    // exp gained from gathering materials from leaves and such
-    expGather
-    getExpGather() {
-    return this.expGather
+    test_Exp() {
+        for (const [key, value] of this.exp) {
+            console.log(`KEY: ${key}`)
+            for (const keys of Object.keys(value)){
+                console.log(`${keys}: ${value[keys]}`)
+            }
+        }
     }
-    setExpGather(value) {
-    this.expGather = value
-    if (this.expGather < 0) this.expGather = 0
-    }
-
-
-    // exp gained from hunting other ants and such
-    expHunt
-    getExpHunt() {
-    return this.expHunt
-    }
-    setExpHunt(value) {
-    this.expHunt = value
-    if (this.expHunt < 0) this.expHunt = 0
-    }
-
-    //exp gained from swimming and water based activities
-    expSwim
-    getExpSwim() {
-    return this.expSwim
-    }
-    setExpSwim(value) {
-    this.expSwim = value
-    if (this.expSwim < 0) this.expSwim = 0
-    }
-
-    // exp gained from an ant revealing parts of the map, discovering new resource, or engaging is "scouting" assigned task
-    expScout
-    getExpScout() {
-    return this.expScout
-    }
-    setExpScout(value) {
-    this.expScout = value
-    if (this.expScout < 0) this.expScout = 0
-    }
-
-
-    // exp gained from an ant farming aphids, harvesting dew from any source
-    expFarming
-    getExpFarming() {
-    return this.expFarming
-    }
-    setExpFarming(value) {
-    this.expFarming = value
-    if (this.expFarming < 0) this.expFarming = 0
-    }
-
-
-    // exp gained from an ant building or repairing any structure
-    expConstruction
-    getExpConstruction() {
-    return this.expConstruction
-    }
-    setExpConstruction(value) {
-    this.expConstruction = value
-    if (this.expConstruction < 0) this.expConstruction = 0
-    }
-
-
-    //exp gained from ants that spits and that successfully connects with an valid object
-    expRanged
-    getExpRanged() {
-    return this.expRanged
-    }
-    setExpRanged(value) {
-    this.expRanged = value
-    if (this.expRanged < 0) this.expRanged = 0
-    }
-
 }
 
 // generic stat that will be used to populate all stats
 class stat {
-    statValue
-    getStatValue() {
-        return this.statValue
-    }
-    setStatValue(value) {
-        this.statValue = value
-        if (this.statValue < 0) this.statValue = 0
-    }
-    changeStatValue(value) {
-        
-    }
-
     //TODO
-    //statUpperLimit, prevents the stat from going too high
-    //statLowerLimit, prevents the stat from foing too low
-    //statName, will be store the name of the state
-
     //Toying with the idea of makeing a dictionary of stats as opposed to a stats class.
 
+    constructor(statName="NONAME",statValue=0,statLowerLimit=0,statUpperLimit=500){
+        this.setStatName(statName)
+        this.setStatValue(statValue)
+        this.setStatLowerLimit(statLowerLimit)
+        this.setStatUpperLimit(statUpperLimit)
+    }
+
+    // will be store the name of the state
+    statName
+    getStatName() { return this.statName }
+    setStatName(value) { this.statName = value  }
+
+    // stores the value of the stat
+    statValue
+    getStatValue() { return this.statValue }
+    setStatValue(value) { this.statValue = value; this.enforceStatLimit() }
+
+    // prevents the stat from going too high
+    statUpperLimit
+    getStatUpperLimit() { return this.statUpperLimit }
+    setStatUpperLimit(value) { this.statUpperLimit = value }
+
+    // prevents the stat from from too low
+    statLowerLimit
+    getStatLowerLimit() { return this.statLowerLimit }
+    setStatLowerLimit(value) { this.statLowerLimit = value }
+
+    // checks that the limits are properly enforced, throws an error 
+    enforceStatLimit() {
+        if (this.statValue < this.statLowerLimit) this.statValue = this.statLowerLimit
+        if (this.statValue > this.statUpperLimit) this.statValue = this.statUpperLimit
+        this.test_enforceStatLimit()
+    }
+    test_enforceStatLimit() {
+        if (this.statValue < this.statLowerLimit) console.error(this.statValue, this.statLowerLimit)
+        if (this.statValue > this.statUpperLimit) console.error(this.statValue, this.statUpperLimit)
+    }
 }
