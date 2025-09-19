19fcec9b2046dce5a38e41f5782aadb29bbf62f6
Colin <crgrant24@gmail.com>
2025-09-19 16:11:20 -0400

    defined NONE and TILE_SIZE

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 let CANVAS_X = 800; // Default 800
 let CANVAS_Y = 800; // Default 800
-let TILE_SIZE = 32; //  Default 32
+const TILE_SIZE = 32; //  Default 32
+const NONE = '\0'; 
 
 let SEED;
 let MAP;
 let GRIDMAP;
-let COORDSY;
+let COORDSY
 
 
 let recordingPath
