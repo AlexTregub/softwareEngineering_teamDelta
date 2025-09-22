dc36bab7caed73d9f96fc2831a7654b0493c92db
Alex Tregub <alexs.tregub@gmail.com>
2025-09-22 16:14:35 -0400

    Placeholder for chunked terrain generation. Need to better define terrain functionality. Chunks are ready, Terrain is not.

---
+++ Classes/terrainUtils/gridTerrain.js
@@ -0,0 +1,7 @@
+///// TERRAIN is GRID of CHUNK is GRID of TILE 
+///// TODO: Define functionality + update coordinate system
+class gridTerrain {
+    constructor(gridSizeX,gridSizeY,gridSpanTLCorner,backingCanvasTLCorner,chunkSize=CHUNK_SIZE) {
+
+    }
+}
