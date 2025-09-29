490b78d3b136466aeedfe360cb171c1634dafc97
Colin <crgrant24@gmail.com>
2025-09-29 12:33:28 -0400

    added new classes for pathfinding

---
+++ Classes/systems/newPathfinding.js
@@ -0,0 +1,72 @@
+class PathMap{
+  constructor(terrain){
+    // this._terrain = terrain; //Requires terrain(for weight, objects, etc.), only used in construction
+    this._grid = new Grid( //Makes Grid for easy tile storage/access
+      terrain._xCount, //Size of terrain to match
+      terrain._yCount,
+      [0,0],
+      [0,0]
+    );
+    for(let y = 0; y < terrain._yCount; y++){
+      for(let x = 0; x < terrain._xCount; x++){
+        let node = new Node(terrain._tileStore[terrain.conv2dpos(x, y)], x, y); //Makes tile out of Tile object
+        this._grid.setArrPos([x, y], node); //Stores tile in grid
+      }
+    }
+    for(let y = 0; y < terrain._yCount; y++){
+      for(let x = 0; x < terrain._xCount; x++){
+        let node = this._grid.getArrPos([x,y]); //Makes tile out of Tile object
+        node.setNeighbors(this._grid); //Stores tile in grid
+      }
+    }
+  }
+  
+  getGrid(){
+    return this._grid;
+  }
+}
+
+class Node{
+  constructor(terrainTile, x, y){
+    this._terrainTile = terrainTile; //Uses Tile and x,y so it can easily find neighbors, know its own location, and have other stuff (know terrain type)
+    this._x = x;
+    this._y = y;
+
+    this.id = `${x}-${y}`; //Used for easier access. Faster than searching 2D array
+    this.neighbors = []; //Traversible neighbors (not walls)
+    this.assignWall();
+    this.weight = this._terrainTile.getWeight();
+    this.reset();
+  }
+  reset(){
+    this.f = 0; //Easier for resetting the map when finished travelling
+    this.g = 0;
+    this.h = 0; //Note, add a function to reset travelled tiles when a path is found
+    this.previousStart = null;
+    this.previousEnd = null;
+  }
+
+  assignWall(){
+    if(this._terrainTile.getWeight() === 100){ //Calls from terrainTile just to avoid possible flip
+      this.wall = true;
+    }
+    else{
+      this.wall = false;
+    }
+  }
+
+  setNeighbors(grid){
+    for(let x = -1; x <= 1; x++){ // Checks previous, current, and next rows/columns
+      for(let y = -1; y <= 1; y++){
+        if (x == 0 && y == 0) continue; //Skips current node
+        let nx = this._x + x;
+        let ny = this._y + y; //Makes sure the sum is within bounds
+        if (nx >= 0 && nx < grid._sizeX && ny >= 0 && ny < grid._sizeY){
+          let neighbor = grid.getArrPos([nx,ny]);
+          this.neighbors.push(neighbor);
+        }
+      }
+    }
+  }
+  //Takes coordinates. If potential neighbor is in bounds, adds it
+}
---
+++ Classes/systems/pheromones.js
@@ -0,0 +1,40 @@
+class Stench{
+    /*Pheromone class:
+        Has state of pheromone: Combat, foraging, etc.
+        Has strength of the pheromone: Queen ant > normal ant. Changes during diffusion
+        Has direction of the pheromone (May add to pathfinding): Where the smell came from. 
+        Has central path marker (Probably in pathfinding): Where the original path is. Ants
+            should follow and branch from the path, not just follow the pheromone everywhere.
+        
+
+    */
+    constructor(state, antType){
+        this.state = state;
+        this.stress = 0;
+        
+    }
+    addStress(terrainType){
+        /*Adding Stress: 
+            When terrain is very difficult to traverse, ants on the path release stress
+            Stress tells ants to find/build another path
+            Slowly evaporates in case difficult terrain is best-case.
+        */
+       
+    }
+}
+
+function diffuse(){
+    /*Diffuse:
+        Diffuses the pheromone: Divides current strength by evaporation rate,
+        Spreads weaker pheromone to other tiles
+        Assuming multiple path tiles are able to diffuse to the same tile
+    */
+}
+
+function findDiffusionRate(){
+    /*Diffusion Rate:
+        Need to account for different terrain modifiers (rough vs flat)
+        Possible
+    */
+    
+}
