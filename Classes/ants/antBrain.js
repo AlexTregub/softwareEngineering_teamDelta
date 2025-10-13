3c10d1f72a6a8500526941db603399373e8fb344
Colin <crgrant24@gmail.com>
2025-10-13 01:33:51 -0400

    added more setters/getters. Forgot a crap ton of this.

--- Classes/ants/antBrain.js
+++ Classes/ants/antBrain.js
@@ -5,63 +5,70 @@ const DEATH = 200;
 class AntBrain{
     constructor(antType){
         this.antType = antType;
-        let flag_ = "";
-        let hunger = 0;
+        this.flag_ = "";
+        this.hunger = 0;
 
-        let followBuildTrail = 0;
-        let followForageTrail = 0;
-        let followFarmTrail = 0;
-        let followEnemyTrail = 0;
-        let followBossTrail = 1;
-        let penalizedTrails = [];
+        this.followBuildTrail = 0;
+        this.followForageTrail = 0;
+        this.followFarmTrail = 0;
+        this.followEnemyTrail = 0;
+        this.followBossTrail = 100;
+        this.penalizedTrails = [];
 
-        setPriority(antType, 1);
+        this.setPriority(antType, 1);
     }
 
     setPriority(antType, mult){
-        job = antType;
+        let job = antType;
         switch (job) {
             case "Builder":
                 this.followBuildTrail = 0.9 * mult;
                 this.followForageTrail = 0.05 * mult;
                 this.followFarmTrail = 0 * mult;
                 this.followEnemyTrail = 0.05 * mult;
+                this.followBossTrail = 100;
                 break;
             case "Scout":
                 this.followBuildTrail = 0.25 * mult;
                 this.followForageTrail = 0.25 * mult;
                 this.followFarmTrail = 0.25 * mult;
                 this.followEnemyTrail = 0.25 * mult;
+                this.followBossTrail = 100;
                 break;
             case "Farmer":
                 this.followBuildTrail = 0 * mult;
                 this.followForageTrail = 0.1 * mult;
                 this.followFarmTrail = 0.85 * mult;
                 this.followEnemyTrail = 0.05 * mult;
+                this.followBossTrail = 100;
                 break;
             case "Warrior":
                 this.followBuildTrail = 0 * mult;
                 this.followForageTrail = 0.2 * mult;
                 this.followFarmTrail = 0 * mult;
                 this.followEnemyTrail = 1 * mult;
+                this.followBossTrail = 100;
                 break;
             case "Spitter":
                 this.followBuildTrail = 0 * mult;
                 this.followForageTrail = 0.2 * mult;
                 this.followFarmTrail = 0 * mult;
                 this.followEnemyTrail = 1 * mult;
+                this.followBossTrail = 100;
                 break;
             case "DeLozier":
                 this.followBuildTrail = 0 * mult;
                 this.followForageTrail = 0 * mult;
                 this.followFarmTrail = 0 * mult;
                 this.followEnemyTrail = 0 * mult;
+                this.followBossTrail = 100;
                 break;
             default:
                 this.followBuildTrail = 0 * mult;
                 this.followForageTrail = 0.75 * mult;
                 this.followFarmTrail = 0 * mult;
                 this.followEnemyTrail = 0.25 * mult;
+                this.followBossTrail = 100;
                 break;
         }
     }
@@ -75,24 +82,50 @@ class AntBrain{
         */
     }
 
-    checkTrail(pheromone, penaltyList, priorityList){
+    checkTrail(pheromone){
         /*Check Trail:
             Checks a trail using trail type and ant type. Generates number from 0-1. If it falls in the bounds, the ant follows the path, else sets to ignore that type until next state change (maybe make it dynamic (-50% chance))
         */
-       let priority = priorityList[somevaluerepresentativeofthelargerprioritywhole];
-       let penalty = penaltyList[somevaluerepresentativeofthelargerprioritywhole];
+       let priority = this.getTrailPriority(pheromone.name);
+       let penalty = this.getPenalty(pheromone.name);
        let num = Math.random();
        let comparison = (pheromone.strength/pheromone.initial) * priority * penalty;
        if(num < comparison){
         return true;
        }
+       this.addPenalty(pheromone.name, 0.5);
        return false;
+    }
+
+    addPenalty(pheromoneName, penaltyValue = 0.5){
+        this.penalizedTrails.push({name: pheromoneName, penalty: penaltyValue}); //Stores penalty and name as a pair
+    }
+
+    getPenalty(pheromoneName) {
+        const penaltyObj = this.penalizedTrails.find(p => p.name === pheromoneName);
+        return penaltyObj ? penaltyObj.penalty : 1; // default penalty = 1 if not found
+    }
+
+    getTrailPriority(trailType){
+        switch(trailType){
+            //Duh ahh return statement. Returns value tied to each priority
+            case "Build":
+                return this.followBuildTrail;
+            case "Forage":
+                return this.followForageTrail;
+            case "Farm":
+                return this.followFarmTrail;
+            case "Enemy":
+                return this.followEnemyTrail;
+            case "Boss":
+                return this.followBossTrail;
+        }
     }
 
     modifyPriorityTrails(){
         switch(this.flag_){
             case "reset":
-                this.setPriority(this.antType, 1); //Change from antType to actual name
+                this.setPriority(this.antType, 1);
                 break;
             case "hungry":
                 this.setPriority(this.antType, 0.5);
@@ -108,40 +141,41 @@ class AntBrain{
     checkHunger(){
         this.hunger++;
         //Checks hunger meter every second
-        if(hunger === HUNGRY){
-            flag_ = "hungry";
-            runFlagState();
+        if(this.hunger === HUNGRY){
+            this.flag_ = "hungry";
+            this.runFlagState();
         }
-        if(hunger === STARVING){
-            flag_ = "starving";
-            runFlagState();
+        if(this.hunger === STARVING){
+            this.flag_ = "starving";
+            this.runFlagState();
         }
-        if(hunger === DEATH){
-            flag_ = "death";
-            runFlagState();
+        if(this.hunger === DEATH){
+            this.flag_ = "death";
+            this.runFlagState();
         }
     }
 
     resetHunger(){
         //Resets hunger once fed (change to use variable if food has different values of saturation)
-        hunger = 0;
-        flag_ = "reset";
+        this.hunger = 0;
+        this.flag_ = "reset";
         this.runFlagState();
     }
     runFlagState(){
         switch(this.flag_){
         case "hungry":
-            modifyPriorityTrails();
+            this.modifyPriorityTrails();
             break;
         case "starving":
-            modifyPriorityTrails();
+            this.modifyPriorityTrails();
             break;
         case "death":
             //killAnt
             break;
         case "reset":
-            modifyPriorityTrails();
-            flag_ = "";
+            this.modifyPriorityTrails();
+            this.flag_ = "";
+            this.penalizedTrails = [];
             break;
         }
     }
