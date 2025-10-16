6ea8889f72acda9069e91c97fbfda0248d6ab777
AlexT <alexs.tregub@gmail.com>
2025-10-16 10:13:39 -0400

    Added simple pheromone 'merge' logic - stronger pheromone/staleness will overwrite. Worst-case storage is now countOfPheromoneTypes*totalCells .

--- Classes/systems/diffusion.js
+++ Classes/systems/diffusion.js
@@ -41,17 +41,6 @@ class PheromoneGrid {
     }
 
     //// Access: (defaults on selected grid)
-    // get(pos,selLeft=this._selLeft) { // Gets current pheromones, in array form
-    //     let rawArr = this.getRaw(pos,selLeft); // Assumes array of pheromone containers
-    //     let returnArr = [];
-
-    //     for (let i = 0; i < rawArr.length; ++i) {
-    //         returnArr.push(rawArr[i].pheromone);
-    //     }
-
-    //     return returnArr;
-    // }
-
     get(pos,selLeft=this._selLeft) { // Gets raw pheromone array at position
         if (selLeft) {
             return this._left.get(pos); 
@@ -60,8 +49,60 @@ class PheromoneGrid {
         return this._right.get(pos);
     }
 
-    push(pos,pheromone,selLeft=this._selLeft) { // Merges pheromone into cell
-        // ...
+    push(pos,pheromone,selLeft=this._selLeft) { // Merges ONE pheromone into cell
+        // ENSURE MERGE LOGIC IS THE SAME IN BOTH...
+        if (selLeft) {
+            if (this._leftSet.has(pos)) { // If exists, merge...
+                // Key exists...
+                let gridAccessPos = this._left.convToFlat(this._left.convPosToArr(pos));
+                let length = this._left.rawArray[gridAccessPos].length;
+
+                for (let i = 0; i < length; ++i) { // Check for matching type, merge if needed and early exit.
+                    if (pheromone.type == this._left.rawArray[gridAccessPos][i].type) {
+                        // CURRENT MERGE LOGIC, LIKELY CHANGED IN FUTURE.
+                        this._left.rawArray[gridAccessPos][i].strength = 
+                            Math.max(pheromone.strength,this._left.rawArray[gridAccessPos][i].strength);
+                        this._left.rawArray[gridAccessPos][i].initial = 
+                            Math.max(pheromone.initial,this._left.rawArray[gridAccessPos][i].initial);
+
+                        return;
+                    }
+                }
+                
+                // Type not exist in cell
+                this._left.rawArray[gridAccessPos].push(pheromone);
+            } else { // Easy set...
+                this._left.set(pos,[pheromone]);
+                this._leftSet.add(pos);
+            }
+
+            return;
+        }
+
+        // selRight
+        if (this._rightSet.has(pos)) { // If exists, merge...
+            // Key exists...
+            let gridAccessPos = this._right.convToFlat(this._right.convPosToArr(pos));
+            let length = this._right.rawArray[gridAccessPos].length;
+
+            for (let i = 0; i < length; ++i) { // Check for matching type, merge if needed and early exit.
+                if (pheromone.type == this._right.rawArray[gridAccessPos][i].type) {
+                    // CURRENT MERGE LOGIC, LIKELY CHANGED IN FUTURE.
+                    this._right.rawArray[gridAccessPos][i].strength = 
+                        Math.max(pheromone.strength,this._right.rawArray[gridAccessPos][i].strength);
+                    this._right.rawArray[gridAccessPos][i].initial = 
+                        Math.max(pheromone.initial,this._right.rawArray[gridAccessPos][i].initial);
+
+                    return;
+                }
+            }
+            
+            // Type not exist in cell, add
+            this._right.rawArray[gridAccessPos].push(pheromone);
+        } else { // Easy set...
+            this._right.set(pos,[pheromone]);
+            this._rightSet.add(pos);
+        }
     }
 
     set(pos,pheromoneArray,selLeft=this._selLeft) { // ...,  selGet=this._selLeft,selPut=this._selLeft) { // OVERWRITE pheromone array
@@ -85,53 +126,10 @@ class PheromoneGrid {
         }
 
         return;
-        
-        // // Delete prev vals from flat array - if needed
-        // if (selPut == selGet) {
-        //     let oldArr = this.getRaw(pos,selGet);
-        //     if (selGet) {
-        //         for (let i = 0; i < oldArr.length; ++i) {
-        //             this._leftFlat[i] = NONE;
-        //         }
-        //     } else {
-        //         for (let i = 0; i < oldArr.length; ++i) {
-        //             this._rightFlat[i] = NONE;
-        //         }
-        //     }
-        // } 
-        
-
-        // // Add new vals to flat + selected position
-        // let appendArray = [];
-        // if (selPut) {
-        //     for (let i = 0; i < pheromoneArray.length; ++i) { // Update flat arr
-        //         appendArray.push(new PheromoneContainer(this._leftFlat.length,pheromoneArray[i]));
-        //         this._leftFlat.push(pheromoneArray[i]);
-        //     }
-
-        //     this._left.set(pos,appendArray);  // Update grid
-
-        //     return;
-        // }
-
-        // for (let i = 0; i < pheromoneArray.length; ++i) { // Update flat arr
-        //     appendArray.push(new PheromoneContainer(this._rightFlat.length,pheromoneArray[i]));
-        //     this._rightFlat.push(pheromoneArray[i]);
-        // }
-
-        // this._right.set(pos,appendArray);  // Update grid
     }
 }
 
 
-
-/// Container for pheromones (handles IDs independently)
-// class PheromoneContainer {
-//     constructor(ID,pheromone) {
-//         this.id = ID;
-//         this.pheromone = pheromone;
-//     }
-// }
 
 class Pos { // Helper object to store in set
     constructor(x,y) {
@@ -140,6 +138,18 @@ class Pos { // Helper object to store in set
 
     toString() { 
         return this.x + "_" +this.y; // coerced to string
+    }
+}
+
+class Pheromone {
+    constructor(type,strength,initial) {
+        this.type = type;
+        this.strength = strength;
+        this.initial = initial;
+    }
+
+    toString() {
+        return this.type+":"+this.strength+'/'+this.initial;
     }
 }
 
